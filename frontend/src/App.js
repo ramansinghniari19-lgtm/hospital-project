dd146c543e663106087df1cf726420c914d9a7d6
Raman Singh <ramansinghniari19@gmail.com>
2026-02-01 16:30:43 +0530

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 // Baaki pages ke components (Jab ban jayenge tab import honge)
-// import Register from "./pages/Register";
+import Register from "../pages/Register";
 // import PatientDashboard from "./pages/PatientDashboard";
 // import DoctorDashboard from "./pages/DoctorDashboard";
 
